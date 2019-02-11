427fdf51c51e2b1dcd8138c49bf4747f854e1528
skesani <kesanisasikanth@gmail.com>
2019-02-11 16:37:51 -0500

    testing

--- src/app/users/user.service.spec.ts
+++ src/app/users/user.service.spec.ts
@@ -1,27 +1,42 @@
-import {inject, TestBed} from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import {UserService} from './user.service';
-import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
-import {HttpClientModule} from '@angular/common/http';
-import {User} from './user';
+import {HttpEvent, HttpEventType} from '@angular/common/http';
 
-describe('Service: UsersService', () => {
-
-    let userService;
-
+describe('UserService', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [UserService],
-            imports: [HttpClientTestingModule, HttpClientModule]
+            imports: [HttpClientTestingModule],
+            providers: [UserService]
         });
     });
-
-    beforeEach(inject([UserService], s => {
-        userService = s;
-    }));
-
-    it('should get all users', () => {
-        userService.getUserDetails(1).subscribe((user: User) => {
-            expect(user[0].email).toBe('aaa');
-        });
-    });
+    it(
+        'should get the list of all users',
+        inject (
+            [HttpTestingController, UserService],
+            (
+                httpMock: HttpTestingController,
+                userService: UserService
+            ) => {
+                userService.getAllUsers().subscribe( event => {
+                    console.log(event);
+                    switch (event.type) {
+                        case HttpEventType.Response:
+                            expect(event.body.length).toBeGreaterThanOrEqual(1);
+                    }
+                });
+                const mockReq = httpMock.expectOne(userService.baseURI + 'users');
+                expect(mockReq.cancelled).toBeFalsy();
+                mockReq.flush({});
+            }
+        )
+    );
+    afterEach(
+        inject(
+            [HttpTestingController],
+            (
+                httpMock: HttpTestingController
+            ) => {
+                httpMock.verify();
+            }));
 });
