e7ecd806b2d8567b1153e0980d29072aa5ad979a
sasikesani <kesanisasikanth@gmail.com>
2019-02-24 13:49:25 -0500

    adding pdf and export functionality

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -3,6 +3,9 @@ import {UserService} from './user.service';
 import {api_error, User} from './user';
 import {AlertService} from '../alert/alert.service';
 import {LoadingService} from '../loading/loading.service';
+import {AngularCsv} from 'angular7-csv';
+import * as Jspdf from 'jspdf';
+import * as html2canvas from 'html2canvas';
 
 @Component({
     selector: 'app-users',
@@ -16,6 +19,18 @@ export class UsersComponent implements OnInit {
     showAllUsers = true;
     showUserDetail = false;
     private apploader;
+
+  csvOptions = {
+    fieldSeparator: ',',
+    quoteStrings: '"',
+    decimalseparator: '.',
+    showLabels: true,
+    showTitle: true,
+    title: 'Your User List :',
+    useBom: true,
+    noDownload: false,
+    headers: ['Name', 'Username', 'Email', 'Phone', 'Website']
+  };
 
     constructor(private userService: UserService, private alertService: AlertService,  private loader: LoadingService) {
         this.apploader = this.loader;
@@ -54,5 +69,33 @@ export class UsersComponent implements OnInit {
             this.apploader.hide();
         });
     }
+
+  downloadCSV() {
+      if (this.users !== null && this.users !== undefined) {
+          const userDetails = [];
+          this.users.forEach(x => {
+            delete  x.address;
+            delete x.company;
+            delete x.id;
+            userDetails.push(x);
+          });
+          const cvs = new AngularCsv(userDetails, 'Users List_' + new Date(), this.csvOptions);
+          console.log(cvs);
+      }
+  }
+
+  downloadPdf() {
+    const data = document.getElementById('userTable');
+    html2canvas(data).then(canvas => {
+      const imgWidth = 208;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+      const contentDataURL = canvas.toDataURL('image/png');
+      const pdf = new Jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+      const position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save('MyUsers_' + new Date() + '.pdf'); // Generated PDF
+    });
+  }
+
 
 }
